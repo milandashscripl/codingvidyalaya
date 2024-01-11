a7c45a8ae59924cd328c5e5b7b1addab82682d4a
milandashscripl <immilankumar01@gmail.com>
2024-01-11 11:43:37 +0530

    disign added

--- dist/codingvidyalay.e31bb0bc.js
+++ dist/codingvidyalay.e31bb0bc.js
@@ -122,11 +122,20 @@ var mytglbtn = document.querySelector(".toggler");
 var mysidebar = document.querySelector(".main_sidebar");
 var mybody = document.querySelector(".main_body");
 var mylogobox = document.querySelector(".logo_box");
+var myproflicon = document.querySelector(".profile");
+var myproflemenu = document.querySelector(".profile_menu");
 mytglbtn.addEventListener("click", function () {
   if (mysidebar.classList.contains("main_sidebar")) {
     mysidebar.classList.replace("main_sidebar", "main_sidebar--show");
   } else {
     mysidebar.classList.replace("main_sidebar--show", "main_sidebar");
+  }
+});
+myproflicon.addEventListener("click", function () {
+  if (myproflemenu.classList.contains("profile_menu")) {
+    myproflemenu.classList.replace("profile_menu", "profile_menu--show");
+  } else {
+    myproflemenu.classList.replace("profile_menu--show", "profile_menu");
   }
 });
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var mytglbtn = document.querySelector(".toggler");
 var mysidebar = document.querySelector(".main_sidebar");
 var mybody = document.querySelector(".main_body")
 var mylogobox = document.querySelector(".logo_box");
+var myproflicon = document.querySelector(".profile");
+var myproflemenu = document.querySelector(".profile_menu")
 
 mytglbtn.addEventListener("click", function(){
     if(mysidebar.classList.contains("main_sidebar")){
@@ -12,4 +14,13 @@ mytglbtn.addEventListener("click", function(){
     }
 });
 
+
+myproflicon.addEventListener("click", function(){
+    if(myproflemenu.classList.contains("profile_menu")){
+        myproflemenu.classList.replace("profile_menu", "profile_menu--show");
+    }
+    else{
+        myproflemenu.classList.replace("profile_menu--show", "profile_menu");
+    }
+})
 
